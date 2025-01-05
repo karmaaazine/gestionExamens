1cb14396ac11777cceec194017c234fc59e138c2
brahimchakir <ibrahimchakir002@gmail.com>
2025-01-05 21:14:17 +0100

    Create database

---
+++ app/Database/Migrations/2025-01-05-192719_CreateRolesTable.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateRolesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE,
+                'auto_increment' => TRUE,
+            ],
+            'role_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 50,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => TRUE,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => TRUE,
+            ],
+        ]);
+        $this->forge->addKey('id', TRUE);
+        $this->forge->createTable('roles');
+
+        // Insert predefined roles
+        $data = [
+            [
+                'role_name' => 'admin',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'role_name' => 'professeur',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'role_name' => 'etudiant',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ];
+        $this->db->table('roles')->insertBatch($data);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('roles');
+    }
+}
