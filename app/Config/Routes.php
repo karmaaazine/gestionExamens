0a2182b9b45e81b8eee7f5c54d3dbafda7cdb3b1
oumaima <oumaimaelbourane@gmail.com>
2025-01-07 12:27:12 +0100

    LoginUserController

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/login', 'LoginController::index');
+$routes->post('/login_process', 'LoginController::login');
+$routes->get('/logout', 'LoginController::logout');
