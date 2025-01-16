06139210aa3a5bad05c23d1e228118a63f46ce59
brahimchakir <ibrahimchakir002@gmail.com>
2025-01-16 22:34:17 +0100

    added new tables to the database

---
+++ app/Database/Migrations/2025-01-16-203740_CreateModulesTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateModulesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('modules');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('modules');
+    }
+}
---
+++ app/Models/ClassesModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ClassesModel extends Model
+{
+    protected $table      = 'classes'; // Table name
+    protected $primaryKey = 'id'; // Primary key
+
+    protected $returnType = 'array'; // Return type
+
+    protected $allowedFields = ['name']; // Fields that can be inserted/updated
+
+    
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+
+    // Validation rules 
+    protected $validationRules = [
+        'name' => 'required|max_length[255]',
+    ];
+
+    protected $validationMessages = [
+        'name' => [
+            'required' => 'The class name is required.',
+        ],
+    ];
+}
